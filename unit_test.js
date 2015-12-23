307974d788b1017256bb2297a720d18353893e05
Sai kumar <sappanab@cisco.com>
2015-12-23 14:53:09 +0530

    Latest updates on unit testing

--- unit_test.js
+++ unit_test.js
@@ -3,9 +3,9 @@ var res = {data:"Testing for mocha"}
 describe('homepage', function(){
   it('should respond to GET',function(done){
     superagent
-      .get('http://localhost:'+3000)
-      .end(function(res){
-        console.log("getting success response >>",res);
+      .get('http://echo.jsontest.com/key/value/one/two')
+      .end(function(err,res){
+        console.log("getting success response >>",res.body);
         done();
     })
   })
